d9cd13c1e38cdf3e3477aa18e4040b5c004de2c5
Roberto Marques <matsufrg@gmail.com>
2021-05-12 13:22:25 -0300

    Second challenge finished

--- lib/reports_generator_challenge.ex
+++ lib/reports_generator_challenge.ex
@@ -35,8 +35,54 @@ defmodule ReportsGeneratorChallenge do
     |> Enum.reduce(report_acc(), fn list, acc -> sum_values(list, acc) end)
   end
 
-  def user_list(param) do
-    Enum.into(@available_names, %{}, &{&1, param})
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Please, provide a list"}
+  end
+
+  def build_from_many(filenames) do
+    result =
+      filenames
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, list}, acc ->
+        sum_many_values(list, acc)
+      end)
+
+    {:ok, result}
+  end
+
+  defp sum_many_values(
+         %{
+           "all_hours" => all_hours1,
+           "hours_per_month" => hours_per_month1,
+           "hours_per_year" => hours_per_year1
+         },
+         %{
+           "all_hours" => all_hours2,
+           "hours_per_month" => hours_per_month2,
+           "hours_per_year" => hours_per_year2
+         }
+       ) do
+    all_hours = Map.merge(all_hours1, all_hours2, fn _k, v1, v2 -> v1 + v2 end)
+
+    hours_per_month = sum_maps(hours_per_month1, hours_per_month2)
+
+    hours_per_year = sum_maps(hours_per_year1, hours_per_year2)
+
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
+  end
+
+  defp sum_maps(map1, map2) when is_map(map1) and is_map(map2) do
+    Map.merge(map1, map2, fn _k, v1, v2 ->
+      sum_maps(v1, v2)
+    end)
+  end
+
+  defp sum_maps(val1, val2) do
+    val1 + val2
   end
 
   defp sum_values([name, hours, _day, month, year], %{
@@ -67,6 +113,10 @@ defmodule ReportsGeneratorChallenge do
       "hours_per_month" => hours_per_month,
       "hours_per_year" => hours_per_year
     }
+  end
+
+  defp user_list(param) do
+    Enum.into(@available_names, %{}, &{&1, param})
   end
 
   def report_acc() do
--- test/reports_generator_challenge_test.exs
+++ test/reports_generator_challenge_test.exs
@@ -2,7 +2,493 @@ defmodule ReportsGeneratorChallengeTest do
   use ExUnit.Case
   doctest ReportsGeneratorChallenge
 
-  test "greets the world" do
-    assert ReportsGeneratorChallenge.hello() == :world
+  describe "build/1" do
+    test "When a file is provided, returns the report" do
+      filename = "gen_report_test.csv"
+
+      response = ReportsGeneratorChallenge.build(filename)
+
+      expected_response = %{
+        "all_hours" => %{
+          "Cleiton" => 19,
+          "Daniele" => 21,
+          "Danilo" => 7,
+          "Diego" => 13,
+          "Giuliano" => 14,
+          "Jakeliny" => 25,
+          "Joseph" => 17,
+          "Mayk" => 33,
+          "Rafael" => 14,
+          "Vinicius" => 0
+        },
+        "hours_per_month" => %{
+          "Cleiton" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 7,
+            "junho" => 4,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 8,
+            "setembro" => 0
+          },
+          "Daniele" => %{
+            "abril" => 7,
+            "agosto" => 0,
+            "dezembro" => 5,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 1,
+            "maio" => 8,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Danilo" => %{
+            "abril" => 1,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 6,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Diego" => %{
+            "abril" => 4,
+            "agosto" => 4,
+            "dezembro" => 1,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 1,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 3
+          },
+          "Giuliano" => %{
+            "abril" => 1,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 9,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 4,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Jakeliny" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 1,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 8,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 14,
+            "novembro" => 0,
+            "outubro" => 2,
+            "setembro" => 0
+          },
+          "Joseph" => %{
+            "abril" => 4,
+            "agosto" => 0,
+            "dezembro" => 2,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 3,
+            "novembro" => 5,
+            "outubro" => 0,
+            "setembro" => 3
+          },
+          "Mayk" => %{
+            "abril" => 4,
+            "agosto" => 0,
+            "dezembro" => 5,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 7,
+            "junho" => 3,
+            "maio" => 0,
+            "março" => 3,
+            "novembro" => 4,
+            "outubro" => 0,
+            "setembro" => 7
+          },
+          "Rafael" => %{
+            "abril" => 0,
+            "agosto" => 7,
+            "dezembro" => 0,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 7,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Vinicius" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          }
+        },
+        "hours_per_year" => %{
+          "Cleiton" => %{
+            2016 => 3,
+            2017 => 0,
+            2018 => 7,
+            2019 => 0,
+            2020 => 9
+          },
+          "Daniele" => %{
+            2016 => 10,
+            2017 => 3,
+            2018 => 7,
+            2019 => 0,
+            2020 => 1
+          },
+          "Danilo" => %{
+            2016 => 0,
+            2017 => 0,
+            2018 => 1,
+            2019 => 6,
+            2020 => 0
+          },
+          "Diego" => %{
+            2016 => 3,
+            2017 => 8,
+            2018 => 1,
+            2019 => 1,
+            2020 => 0
+          },
+          "Giuliano" => %{
+            2016 => 0,
+            2017 => 3,
+            2018 => 0,
+            2019 => 6,
+            2020 => 5
+          },
+          "Jakeliny" => %{
+            2016 => 8,
+            2017 => 8,
+            2018 => 0,
+            2019 => 7,
+            2020 => 2
+          },
+          "Joseph" => %{
+            2016 => 0,
+            2017 => 3,
+            2018 => 0,
+            2019 => 3,
+            2020 => 11
+          },
+          "Mayk" => %{
+            2016 => 11,
+            2017 => 8,
+            2018 => 0,
+            2019 => 7,
+            2020 => 7
+          },
+          "Rafael" => %{
+            2016 => 0,
+            2017 => 14,
+            2018 => 0,
+            2019 => 0,
+            2020 => 0
+          },
+          "Vinicius" => %{
+            2016 => 0,
+            2017 => 0,
+            2018 => 0,
+            2019 => 0,
+            2020 => 0
+          }
+        }
+      }
+
+      assert expected_response == response
+    end
+  end
+
+  describe "build_from_many/1" do
+    test "When a list is provided, returns the report" do
+      filenames = ["gen_report_test.csv", "gen_report_test.csv"]
+
+      response = ReportsGeneratorChallenge.build_from_many(filenames)
+
+      expected_response =
+        {:ok,
+         %{
+           "all_hours" => %{
+             "Cleiton" => 38,
+             "Daniele" => 42,
+             "Danilo" => 14,
+             "Diego" => 26,
+             "Giuliano" => 28,
+             "Jakeliny" => 50,
+             "Joseph" => 34,
+             "Mayk" => 66,
+             "Rafael" => 28,
+             "Vinicius" => 0
+           },
+           "hours_per_month" => %{
+             "Cleiton" => %{
+               "abril" => 0,
+               "agosto" => 0,
+               "dezembro" => 0,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 14,
+               "junho" => 8,
+               "maio" => 0,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 16,
+               "setembro" => 0
+             },
+             "Daniele" => %{
+               "abril" => 14,
+               "agosto" => 0,
+               "dezembro" => 10,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 2,
+               "maio" => 16,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             },
+             "Danilo" => %{
+               "abril" => 2,
+               "agosto" => 0,
+               "dezembro" => 0,
+               "fevereiro" => 12,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             },
+             "Diego" => %{
+               "abril" => 8,
+               "agosto" => 8,
+               "dezembro" => 2,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 0,
+               "maio" => 2,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 6
+             },
+             "Giuliano" => %{
+               "abril" => 2,
+               "agosto" => 0,
+               "dezembro" => 0,
+               "fevereiro" => 18,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 0,
+               "maio" => 8,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             },
+             "Jakeliny" => %{
+               "abril" => 0,
+               "agosto" => 0,
+               "dezembro" => 2,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 16,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 28,
+               "novembro" => 0,
+               "outubro" => 4,
+               "setembro" => 0
+             },
+             "Joseph" => %{
+               "abril" => 8,
+               "agosto" => 0,
+               "dezembro" => 4,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 6,
+               "novembro" => 10,
+               "outubro" => 0,
+               "setembro" => 6
+             },
+             "Mayk" => %{
+               "abril" => 8,
+               "agosto" => 0,
+               "dezembro" => 10,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 14,
+               "junho" => 6,
+               "maio" => 0,
+               "março" => 6,
+               "novembro" => 8,
+               "outubro" => 0,
+               "setembro" => 14
+             },
+             "Rafael" => %{
+               "abril" => 0,
+               "agosto" => 14,
+               "dezembro" => 0,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 14,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             },
+             "Vinicius" => %{
+               "abril" => 0,
+               "agosto" => 0,
+               "dezembro" => 0,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             }
+           },
+           "hours_per_year" => %{
+             "Cleiton" => %{
+               2016 => 6,
+               2017 => 0,
+               2018 => 14,
+               2019 => 0,
+               2020 => 18
+             },
+             "Daniele" => %{
+               2016 => 20,
+               2017 => 6,
+               2018 => 14,
+               2019 => 0,
+               2020 => 2
+             },
+             "Danilo" => %{
+               2016 => 0,
+               2017 => 0,
+               2018 => 2,
+               2019 => 12,
+               2020 => 0
+             },
+             "Diego" => %{
+               2016 => 6,
+               2017 => 16,
+               2018 => 2,
+               2019 => 2,
+               2020 => 0
+             },
+             "Giuliano" => %{
+               2016 => 0,
+               2017 => 6,
+               2018 => 0,
+               2019 => 12,
+               2020 => 10
+             },
+             "Jakeliny" => %{
+               2016 => 16,
+               2017 => 16,
+               2018 => 0,
+               2019 => 14,
+               2020 => 4
+             },
+             "Joseph" => %{
+               2016 => 0,
+               2017 => 6,
+               2018 => 0,
+               2019 => 6,
+               2020 => 22
+             },
+             "Mayk" => %{
+               2016 => 22,
+               2017 => 16,
+               2018 => 0,
+               2019 => 14,
+               2020 => 14
+             },
+             "Rafael" => %{
+               2016 => 0,
+               2017 => 28,
+               2018 => 0,
+               2019 => 0,
+               2020 => 0
+             },
+             "Vinicius" => %{
+               2016 => 0,
+               2017 => 0,
+               2018 => 0,
+               2019 => 0,
+               2020 => 0
+             }
+           }
+         }}
+
+      assert expected_response == response
+    end
+
+    test "When an list is not provided, returns the error" do
+      filenames = "Test"
+
+      response = ReportsGeneratorChallenge.build_from_many(filenames)
+
+      expected_response = {:error, "Please, provide a list"}
+
+      assert expected_response == response
+    end
   end
 end
